1193ac04bd3fa08be947caa93f2bb2ea14464565
Ramasubramaniyam Azhagusundaram <rmsubramaniyam@gmail.com>
2024-01-06 11:02:07 -0800

    Lowere case func

--- sub02.go
+++ sub02.go
@@ -1,3 +1,10 @@
 package sub02
 
+import "strings"
+
 const Desc string = "** Lovely compute **"
+const Desc1 string = "** Same word in Lowercase **"
+
+func Lowerconv(s string) string {
+	return "Desc1" + strings.ToLower(s)
+}
